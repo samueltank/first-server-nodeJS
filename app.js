7670397ff2df614e1147b277dcb7afe940d9a66f
Samuel Tank <dev.samueltank@gmail.com>
2022-03-25 10:58:24 -0300

    Start Repo

---
+++ app.js
@@ -0,0 +1,15 @@
+/* Esse eu fiz na mão kkk */
+
+'use strict'; /* modo restrito ativado! */
+
+const express = require('express');       /* pega o módulo Express do NPM */
+const app     = express();                /* cria uma aplicação express */
+const port    = process.env.PORT || 3000; /* define a porta a ser utilizada */
+
+/* rota a ser utilizada */
+app.get('/', (req, res) => {
+  /* callback de processamento da requisição */
+  return res.send('Hello World!');
+});
+Creates an Express application. The express() function is a top-level function exported by the express module.
+app.listen(port); /* inicia a aplicação na porta definida */
